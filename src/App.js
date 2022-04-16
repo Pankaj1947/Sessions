946d036d3ca9e225909ad9642b296a73ceed4d43
Pankaj <pankajkr885@gmail.com>
2022-04-16 22:36:33 +0530

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
 import D5Counter from './Day5Useeffect/PreClass/D5Counter';
+import { D5Demo } from './Day5Useeffect/PreClass/D5Demo';
 
 
 
 
 function App() {
+  const [showCounter,setShowCounter]=React.useState(false);
+
 
   return (
     <div className='App'>
-      <D5Counter/>
+      {showCounter && <D5Counter/>}
+      <br/>
+      <button onClick={()=>setShowCounter(!showCounter)}>SHOW</button>
+      <D5Demo/>
     </div>
   )
 };
--- src/Day5Useeffect/PreClass/D5Counter.jsx
+++ src/Day5Useeffect/PreClass/D5Counter.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function D5Counter() {
     const [count, setCount] = React.useState(10);
 
-    React.useState(() => {
+    React.useEffect(() => {
         const id = setInterval(() => {
+            console.log("inside set Interval");
             setCount((prev) => {
                 if (prev === 0) {
                     clearInterval(id);
@@ -13,11 +14,24 @@ export default function D5Counter() {
                 return prev - 1;
             })
         }, 1000)
+        //cleanup
+        return () => {
+            console.log("cleaning up any setIntervals");
+            clearInterval(id);
+        };
     }, []);
+
 
     return (
         <>
-        <h1>Counter {count}</h1>
+            <h1>Counter {count}</h1>
         </>
     )
 }
+
+//useeffect has an empty dependency
+// it willl get called when the component get unmounted 
+
+//if it has a dependency, count
+// it will get called, everytime count get updated,
+//but before the useeffect callback is triggered 
