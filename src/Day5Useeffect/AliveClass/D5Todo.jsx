18f25f7959971276bf4da1c8f2d7b0edc8592cd8
Pankaj <pankajkr885@gmail.com>
2022-04-17 20:17:12 +0530

    Update D5Todo.jsx

--- src/Day5Useeffect/AliveClass/D5Todo.jsx
+++ src/Day5Useeffect/AliveClass/D5Todo.jsx
@@ -1,11 +1,28 @@
-import {useEffect} from "react";
-export const D5Todo=()=>{
+import { useEffect, useState } from "react";
+
+
+export const D5Todo = () => {
+
+    const [counter1, setCounter1] = useState(0);
+
+    const [counter2, setCounter2] = useState(0);
+
     console.log("Before Effect")
-useEffect(()=>{
-    console.log("Inside Effect")
-})
+    useEffect(() => {
+        console.log("Inside Effect 1")
+    }, [counter1])
+    useEffect(() => {
+        console.log("Inside Effect 2")
+    }, [counter2])
 
     console.log("After Effect")
 
-    return <div>Todos:</div>
+    return <div>Todos:
+        <button onClick={()=>{
+            setCounter1(counter1+1)
+        }}>ADD1</button>
+        <button onClick={() => {
+            setCounter2(counter2 + 1)
+        }}>ADD2</button>
+    </div>
 }
