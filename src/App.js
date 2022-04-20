4a62ebcf87e0dd0e7bb663a1187ca898ac0a5a7e
Pankaj <pankajkr885@gmail.com>
2022-04-21 01:00:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import { D6Form } from './Day6/AliveClass/D6Form';
 
 
 
 function App() {
   return (
     <div className='App'>
-      
+      <D6Form/>
     </div>
   )
 };
