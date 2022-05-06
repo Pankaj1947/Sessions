c09ad54e24fde3cc0280fda582fc7ea8bab1c51d
Pankaj <pankajkr885@gmail.com>
2022-05-06 12:11:56 +0530

    payment card created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { PaymentCard } from './Project/PaymentCard';
+import { Address } from './Project/Address';
 
 function App() {
 
   return (
 
     <div>
-
+     
+      <Address />
     </div>
   )
 }
